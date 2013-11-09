807cd10f4eac26da668bc7c0804d07ed859f9fb4
Chris Wilhelm <cwilhelm@smu.edu>
2013-11-08 22:07:18 -0600

    Revised kernel.c for readSector function.

--- compileOS.sh
+++ compileOS.sh
@@ -1,5 +1,9 @@
 rm -f kernel.o
 bcc -ansi -c -o kernel.o kernel.c
 ld86 -o kernel -d kernel.o kernel_asm.o
+if [ ! -f kernel ]; then
+	exit 1
+fi
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
+dd if=message.txt of=floppya.img bs=512 count=1 seek=30 conv=notrunc
 bochs -f opsys.bxrc
--- kernel.c
+++ kernel.c
@@ -6,45 +6,67 @@
    Chris Wilhelm
 */
 
+#define IO_INTERRUPT        0x10
+#define SECTOR_RW_INTERRUPT 0x13
+#define KEYBOARD_INTERRUPT  0x16
+
+#define AH_OUT_TO_CONSOLE  0x0E
+
+#define ENTER_ASCII        0x0D
+#define BKSPC_ASCII        0x08
+
+#define LINE_FEED          0x0A
+#define END_OF_STRING      0x00
+
 void printString(char*);
 void printChar(char);
 void readString(char*);
+void readSector(char*, int);
+int mod(int a, int b);
+int div(int a, int b);
 
 int main() {
    char line[80];
-
+   char buffer[512];
+   /*
    // Printing to the screen using interrupts
    printString("Enter a line: \0");
    readString(line);
    printString(line);
+*/
+   // Read message from sector 30
+   readSector(buffer, 30);
+   printString(buffer);
+
    while(1);
-   return 0;
 }
 
 void printString(char *string) {
    int i = 0;
-   
+   int ax;
    // parse the array until null char is found
    while ( string[i] != '\0' )
    {
-      interrupt(0x10, 0xe * 256 + string[i], 0, 0, 0);
+      ax = AH_OUT_TO_CONSOLE * 256 + string[i];
+      interrupt(IO_INTERRUPT, ax, 0, 0, 0);
       i++;
    }
 }
 
 void printChar(char character) {
-   interrupt(0x10, 0xe * 256 + character, 0, 0, 0);
+   int ax = AH_OUT_TO_CONSOLE * 256 + character;
+   interrupt(IO_INTERRUPT, ax, 0, 0, 0);
 }
 
 void readString(char line[80]) {
    char key;
    int i = 0;
 
-   key = interrupt(0x16, 0, 0, 0, 0);
-   while ( key != 0xd )
+   key = interrupt(KEYBOARD_INTERRUPT, 0, 0, 0, 0);
+   while ( key != ENTER_ASCII )
    {
       // Handle backspace key press
-      if ( key == 0x8 )
+      if ( key == BKSPC_ASCII && i > 0 )
       {
          i--;
          printString("-BKSPC-");
@@ -55,13 +77,49 @@ void readString(char line[80]) {
          line[i] = key;
          i++;
       }
-      key = interrupt(0x16, 0, 0, 0, 0);
+      key = interrupt(KEYBOARD_INTERRUPT, 0, 0, 0, 0);
    }
-   key = 0xa;
+   key = LINE_FEED;
    printChar(key);
    line[i] = key;
    i++;
-   key = 0x0;
+   key = END_OF_STRING;
    printChar(key);
    line[i] = key;
+}
+
+void readSector(char buffer[512], int sector)
+{
+   int ah = 2; // read sector
+   int al = 1; // number of sectors to read
+   int ax = ah * 256 + al;
+
+   int ch = div(sector, 36); // track number
+   int cl = mod(sector, 18) + 1; // relative sector
+   int cx = ch * 256 + cl;
+
+   int dh = mod(div(sector, 18), 2); // head number
+   int dl = 0; // device number
+   int dx = dh * 256 + dl;
+
+   interrupt(SECTOR_RW_INTERRUPT, ax, buffer, cx, dx);
+}
+
+int mod(int a, int b)
+{
+   while ( a >= b )
+   {
+      a = a - b;
+   }
+   return a;
+}
+
+int div(int a, int b)
+{
+   int quotient = 0;
+   while ( (quotient * b) < a )
+   {
+      quotient++;
+   }
+   return quotient - 1;
 }
